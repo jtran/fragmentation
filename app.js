4caf01d0214ed3b37910587b194dc53c5648715a
Jonathan Tran <jonnytran@gmail.com>
2011-09-26 00:01:05 -0400

    Moved prototype code to serve via express at /.

--- app.js
+++ app.js
@@ -28,11 +28,5 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
-  });
-});
-
 app.listen(3001);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
