11aaa04ab4a58ff87d953189238af3103fd04c27
Jonathan Tran <jonnytran@gmail.com>
2011-09-26 02:03:23 -0400

    Added chat with nowjs.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
  */
 
 var express = require('express');
+var nowjs = require('now');
 
 var app = module.exports = express.createServer();
 
@@ -30,3 +31,17 @@ app.configure('production', function(){
 
 app.listen(3001);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+var everyone = nowjs.initialize(app);
+
+nowjs.on("connect", function(){
+  console.log("Joined: " + this.now.name);
+});
+
+nowjs.on("disconnect", function(){
+  console.log("Left: " + this.now.name);
+});
+
+everyone.now.distributeMessage = function(msg) {
+  everyone.now.receiveMessage(this.user.clientId, this.now.name, msg);
+};
